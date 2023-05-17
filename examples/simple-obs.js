51b7e3d4896c069f73f1edea704c614b2c10e491
boblongest <longestr@gmail.com>
2023-05-17 17:02:51 +0000

    updating to time series example

--- examples/simple-obs.js
+++ examples/simple-obs.js
@@ -1,4 +1,4 @@
-import { from, filter, map } from 'rxjs';
+import { from, filter, map, delay, interval, take } from 'rxjs';
 
 // Example 1
 console.log("Example 1 with a single operator");
@@ -11,11 +11,12 @@ from(['A','B','C'])
 });
 
 // Example 2
-console.log("Example 2 with multiple operators");
-from(['A','B','C'])
-  .pipe(                      // put together a list of operators (functions)
-    filter(x => x != 'B'),    // filter out the letter 'B'
-    map(y => y + 1)           // concat the number 1 to each item in the list
+console.log("Example 2 with multiple operators showing a time series");
+interval(1000)
+  .pipe(   
+    take(4),                  // put together a list of operators (functions)
+    filter(x => x != '2'),    // filter out the letter 'B'
+    map(y => "" + y + 'A'),   // concat the number 1 to each item in the list
   )
   .subscribe((response) => {  // call for the results of the observable and print
     console.log(response)
