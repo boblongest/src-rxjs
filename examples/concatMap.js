23f4a0218092268e9d4d950db10f6668a69e15e2
boblongest <longestr@gmail.com>
2023-05-17 14:20:31 +0000

    cleaning up text

--- examples/concatMap.js
+++ examples/concatMap.js
@@ -1,27 +1,29 @@
 import { mergeMap, concatMap, from } from 'rxjs';
 import axios from 'axios';
 
+const headers = { headers: { 'Accept-Encoding': 'application/json', } }
+
 const urls = [
   'https://pokeapi.co/api/v2/pokemon/bulbasaur',
   'https://pokeapi.co/api/v2/pokemon/weedle',
   'https://pokeapi.co/api/v2/pokemon/charmander',
 ];
 
-// Example 1 - with concatMap URL request");
+// Example 1 - with concatMap URL request
 from(urls)
   .pipe(
     concatMap((url) => {
-      return axios.get(url, { headers: { 'Accept-Encoding': 'application/json', } })
+      return axios.get(url, headers)
     })
   ).subscribe((response) => { 
     console.log("concatMap:" + response.data.name)
   });
 
-// Example 2 - with mergeMap URL request");
+// Example 2 - with mergeMap URL request
 from(urls)
   .pipe(
     mergeMap((url) => {
-      return axios.get(url, { headers: { 'Accept-Encoding': 'application/json', } })
+      return axios.get(url, headers)
     })
   ).subscribe((response) => { 
     console.log("mergeMap:" + response.data.name)
--- examples/mergeMap.js
+++ examples/mergeMap.js
@@ -1,27 +1,29 @@
 import { mergeMap, from } from 'rxjs';
 import axios from 'axios';
 
+const headers = { headers: { 'Accept-Encoding': 'application/json', } }
+
 const urls = [
   'https://pokeapi.co/api/v2/pokemon/bulbasaur',
   'https://pokeapi.co/api/v2/pokemon/weedle',
   'https://pokeapi.co/api/v2/pokemon/charmander',
 ];
 
-// Example 1 - with a single URL request");
+// Example 1 - with a single URL request
 from(urls)
   .pipe(
     mergeMap((url) => {
-      return axios.get(url, { headers: { 'Accept-Encoding': 'application/json', } })
+      return axios.get(url, headers)
     })
   ).subscribe((response) => { 
     console.log("mergeMap 1:" + response.data.name)
   });
   
-// Example 2 - with a chained URL request");
+// Example 2 - with a chained URL request
 from(urls)
   .pipe(
     mergeMap((url) => {
-      return axios.get(url, { headers: { 'Accept-Encoding': 'application/json', } })
+      return axios.get(url, headers)
     }),
     mergeMap((poke) => {
       return axios.get(`https://pokeapi.co/api/v2/ability/${poke.data.abilities[0].ability.name}`, { headers: { 'Accept-Encoding': 'application/json', } })
